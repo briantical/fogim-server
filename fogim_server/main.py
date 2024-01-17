2a05995338f7a0818d55c6feca08ecf293e60672
briantical <lutbrianivan@gmail.com>
2024-01-17 03:44:12 +0300

    Servig static files

--- fogim_server/main.py
+++ fogim_server/main.py
@@ -1,7 +1,11 @@
 from typing import Union
 from fastapi import FastAPI
 
+from fastapi.staticfiles import StaticFiles
+
 app = FastAPI()
+
+app.mount("/media", StaticFiles(directory="media"), name="media")
 
 
 @app.get("/")
